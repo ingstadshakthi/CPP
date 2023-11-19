f3a33f45cec8d9afdd115a3e4009a5218edde79c
Shakthivel S <59392423+ingstadshakthi@users.noreply.github.com>
2023-11-19 23:43:18 +0530

    Create GFG-Count_the_Reversals.cpp

---
+++ Stack/GFG-Count_the_Reversals.cpp
@@ -0,0 +1,31 @@
+int countRev (string s)
+{
+    if(s.size() & 1 == 1) return -1;
+    
+    stack<char> st;
+    int ans = 0;
+    for(char ch:s){
+        if(ch == '{'){
+            st.push(ch);
+        }else{
+            if(!st.empty() && st.top() == '{'){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+    }
+    
+    while(!st.empty()){
+        char a = st.top();
+        st.pop();
+        char b = st.top();
+        st.pop();
+        if(a == b)
+            ans +=1;
+        else
+            ans +=2;
+    }
+    return ans;
+    // your code here
+}
