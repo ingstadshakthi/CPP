2b8a431e91ccffc4f6e461442e4eefc9c7c93d10
Shakthivel S <59392423+ingstadshakthi@users.noreply.github.com>
2023-11-25 23:16:03 +0530

    Create reverse.cpp

---
+++ Queue/reverse.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+void reverseQueue(queue<int> & q){
+  if(q.empty())
+    return;
+  int temp = q.front();
+  q.pop();
+  reverseQueue(q);
+  q.push(temp);
+}
+int main() {
+  queue<int> q;
+
+  q.push(10);
+  q.push(20);
+  q.push(30);
+  q.push(40);
+  q.push(50);
+  q.push(60);
+  reverseQueue(q);
+
+  while(!q.empty()){
+    cout<<q.front()<<" ";
+    q.pop();
+  }
+  cout<<endl;
+  return 0;
+}
